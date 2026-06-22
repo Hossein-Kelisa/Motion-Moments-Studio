09742d87903b77ee6034c5d981e1417c9f9bd6e0
Hossein <Hossein.kelisa@gmail.com>
2026-06-22 15:52:09 +0200

    add hero component in homepage

---
+++ client/src/components/homeComponents/Hero.jsx
@@ -0,0 +1,50 @@
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <section className="hero-section">
+      {/* Background Video */}
+      <video
+        className="hero-video"
+        src="/Hero-vid.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+      />
+      <img
+        className="hero-image"
+        src="/Hero-img.jpg"
+        alt="Hero Background"
+      />
+
+      {/* Overlay */}
+      <div className="hero-overlay"></div>
+
+      {/* Content */}
+      <div className="hero-content">
+        <h1>
+          Capturing Moments with
+          <span> M&M Studio</span>
+        </h1>
+
+        <p>
+          Professional Photography & Videography for Weddings,
+          Events, and Brands.
+        </p>
+
+        <div className="hero-buttons">
+          <button className="hero-btn-portfolio">
+            View Portfolio
+          </button>
+
+          <button className="hero-btn-book">
+            Book a Session
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Hero from "../components/homeComponents/Hero";
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+};
+
+export default Home;
