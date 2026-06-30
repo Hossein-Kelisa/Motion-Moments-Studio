69b6cc79c7ebeac3a44004e268c763e8a9b35e8e
mac <mac@hosseins-mbp.home>
2026-06-30 23:47:21 +0200

    install new npm because I transfered all my projects to new laptop

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,14 +4,12 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header-container">
-        {/* Logo */}
         <div className="logo">
           <a href="/">
             <img className="logo-img" src="/M&M logo4.png" alt="Logo" />
           </a>
         </div>
 
-        {/* Navbar */}
         <nav className="nav">
           <ul>
             <li>
--- client/src/components/homeComponents/Hero.jsx
+++ client/src/components/homeComponents/Hero.jsx
@@ -1,47 +1,27 @@
-import "./Hero.css";
+// import "./Hero.css";
 
 const Hero = () => {
   return (
     <section className="hero-section">
-      {/* Background Video */}
-      <video
-        className="hero-video"
-        src="/Hero-vid2.mp4"
-        autoPlay
-        loop
-        muted
-        playsInline
-      />
-      <img
-        className="hero-image"
-        src="/Hero-img.jpg"
-        alt="Hero Background"
-      />
-
-      {/* Overlay */}
-      <div className="hero-overlay"></div>
-
-      {/* Content */}
-      <div className="hero-content">
+      <div className="hero-left">
         <h1>
           Capturing Moments with
           <span> M&M Studio</span>
         </h1>
 
         <p>
-          Professional Photography & Videography for Weddings,
-          Events, and Brands.
+          Professional Photography & Videography for Weddings, Events, and
+          Brands.
         </p>
 
         <div className="hero-buttons">
-          <button className="hero-btn-portfolio">
-            View Portfolio
-          </button>
+          <button className="hero-btn-portfolio">View Portfolio</button>
 
-          <button className="hero-btn-book">
-            Book a Session
-          </button>
+          <button className="hero-btn-book">Book a Session</button>
         </div>
+      </div>
+      <div className="hero-right">
+        <img className="hero-image" src="/Hero-img.jpg" alt="Hero Background" />
       </div>
     </section>
   );
